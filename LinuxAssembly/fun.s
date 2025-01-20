7b8ff6cda78d769151463a7a756fb6d8591f5d93
ADKeiber <adkeiber@gmail.com>
2025-01-19 22:56:01 -0500

    functions

--- LinuxAssembly/fun.s
+++ LinuxAssembly/fun.s
@@ -3,13 +3,25 @@ section .data
 section .text
 global main
 
-addTwo: 
+addTwo:
+   ; stack setup
+   PUSH ebp       ;EBP is used as a sort of divider in the stack to tell what belongs to certain methods
+   MOV ebp, esp
+
+   ;actual method
+   MOV eax, [ebp + 8]
+   MOV ebx, [ebp + 12]
    ADD eax, ebx
+
+   ;stack cleanup
+   POP ebp
+
    RET
 
 main:
-   MOV eax, 4
-   MOV ebx, 1
+   PUSH 4
+   PUSH 1
+
    CALL addTwo
    MOV ebx, eax
    MOV eax, 1
