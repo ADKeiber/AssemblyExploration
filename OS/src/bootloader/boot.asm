1fe96cf00076f3b29f95ac0ee07fed9348c69871
ADKeiber <adkeiber@gmail.com>
2025-01-23 09:38:33 -0500

    reading from disk

--- OS/src/bootloader/boot.asm
+++ OS/src/bootloader/boot.asm
@@ -33,6 +33,13 @@ main:
    ; All are init to 0
 
    MOV sp, 0x7C00 ; Stets the stack to the starting point of the application so we can 'grow' the stack downwards
+
+   MOV [ebr_drive_number], dl
+   MOV ax, 1
+   MOV cl, 1
+   MOV bx, 0x7e00 ; Buffer located on the disk
+   call disk_read
+
    MOV si, os_boot_msg
    CALL print
    
@@ -40,6 +47,80 @@ main:
 
 halt:
    JMP halt
+
+   ; input: LBA index in ax
+   ; cx [bits 0-5]: sector number
+   ; cx [bits 6-15]: cylinder
+   ; dh: head
+lba_to_chs:
+   PUSH ax
+   PUSH dx
+
+   XOR dx, dx
+   DIV word [bdb_secotrs_per_track] ; LBA % secotrs per track + 1 <- sector
+   INC dx   ; sector
+   MOV cx, dx
+
+   XOR dx, dx ; clear dx
+   DIV word [bdb_heads]
+
+   MOV dh, dl ; head: (LBA / secotrs per track) % number of heads
+   MOV ch, al
+   SHL ah, 6
+   OR cl, ah   ; moves bits to proper location.. cylinder: (LBA / sectors per track)/ number of heads
+
+   POP ax
+   MOV dl, al
+   POP ax
+   RET
+
+disk_read:
+   ; converts lba value into chs format, then tries to read using interrupt 13
+   ; perserve data
+   PUSH ax
+   PUSH bx
+   PUSH cx
+   PUSH dx
+   PUSH di
+
+   call lba_to_chs ; formats from LBA (logigical block addressing) to chs (cylinder, head, and sector)
+
+   MOV ah, 02h
+   MOV di, 3   ;counter for loop
+
+retry:
+   STC
+   INT 13h
+   jnc doneRead
+   
+   call diskReset
+
+   DEC di
+   TEST di,di
+   JNX retry
+
+failDiskRead:
+   MOV si, read_failure
+   CALL print
+   HLT
+   JMP halt
+
+diskReset:
+   PUSHA ; push all 16-bit general purpose registers on stack
+   MOV ah, 0
+   STC
+   INT 13h
+   JC failDiskRead
+   POPA
+   RET
+
+doneRead:
+   pop di
+   pop dx
+   pop cx
+   pop bx
+   pop ax
+   RET
 
 print:
    PUSH si
@@ -64,7 +145,7 @@ done_print:
    RET
 
 os_boot_msg: DB 'Our OS has booted!', 0x0D, 0x0A, 0 ; new line character and sentinel value
-
+read_failure DB 'Failed to read disk!', 0x0D, 0x0A, 0
 TIMES 510-($-$$) DB 0 ; '$-$$' gives the the size of the application
 DW 0AA55h
 
