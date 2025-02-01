c8619a43dfba703745194c6711be257ddee158cd
Austin Keiber <adkeiber@gmail.com>
2025-02-01 10:20:32 -0500

    reading from the disk in bios

--- OS2/src/bootloader/boot.asm
+++ OS2/src/bootloader/boot.asm
@@ -36,12 +36,116 @@ main:
     ; resets registers for bootloader
 
     MOV sp, 0x7C00 ;sets the stack bottom to be after our application code (0x7C00) so we can grow downwards!
+
+    ; Performing/setting up call for read disk sectors here https://stanislavs.org/helppc/int_13-2.html
+    MOV [ebr_drive_number], dl  ; Moves value dl into address of EBR_drive_number
+    MOV ax, 1                   ; LBA index converting to CHS
+    MOV cl, 1                   ; Sector number
+    MOV bx, 0x7E00              ; Location of a buffer that we KNOW exists
+    call disk_read
+
+
     MOV si, os_boot_msg ; moves the os boot message address into the si register
     CALL print
     HTL     ; Pauses the CPU until a certain interrupt that occurs on the system
 
 halt:
     JMP halt
+
+; Converts LBA to CHS. 
+; Input is LBA index in ax.
+; Outputs: 
+; cx [bits 0 - 5]: sector number 
+; output cx [bits 6 - 15] cylinder  
+; dh: head
+lba_to_chs:
+
+    ; Using these registers here.. Perserving data
+    PUSH ax
+    PUSH dx
+
+    XOR dx, dx ; Zeroing out dx
+
+    ; (LBA % sectors per track) + 1 <- sector
+    DIV word [bdb_sectors_per_track] 
+    INC dx      ; Sector
+    mov cx, dx
+
+    XOR dx, dx  ; clear dx (its now stored in cx)
+
+    ; Head: (LBA / sectors per track) % number of heads. (LBA / sectors per track) IS STORED IN THE AX REGISTER RIGHT NOW!!!
+    DIV word [bdb_heads]
+    MOV dh, dl  ; Moves head into proper register (DH)
+
+    ; copies data from al, which contains the sector number, into ch then shifts that data 6 bits and ors it out which esentially empties the 
+    ; lower part of the CH register (CL) but keeps the data that was previously in CL in the higher order bits of CH (sector number)
+    MOV ch, al
+    SHL ah, 6
+    OR cl, ah ; cylinder: (LBA / sectors per track) / number of heads
+
+    ; Now CX and DH both have the desired data in them!
+    ;Restores the registers
+    POP ax
+    MOV dl, al
+    POP ax
+
+    RET
+
+; Read disk sectors interrupt https://stanislavs.org/helppc/int_13-2.html
+disk_read: ; Converts lba value to chs format. Tries to read using interrupt 13 
+
+    ; Perserving registers that we are going to use
+    PUSH ax
+    PUSH bx
+    PUSH cx
+    PUSH dx
+    PUSH di
+
+    call lba_to_chs
+
+    MOV ah, 02h
+    MOV di, 3       ;Counter to loop (for this interrupt we are supposted to try up to 3 times)
+
+retry:
+    STC ;sets the carry!
+    INT 13h
+    jnc doneRead
+
+    call diskReset
+
+    ; loops if di is not zero
+    DEC di
+    TEST di, di
+    JNZ retry 
+
+; Prints failure message to screen and halts
+failDiskRead:
+    MOV si, read_failure
+    CALL print
+    HTL
+    JMP halt
+
+diskReset: ; resets the disk so we can attempt to read again. This is required and defined in docs
+    ; perserve registers. push a pushes all general purpose register data onto the stack
+    PUSHA
+
+    MOV ah, 0   ; Resetting disk system
+    STC         ; we need this command incase the bios doesn't set the carry. This makes sure it does
+    INT 13h
+    JC failDiskRead
+    POPA ; restores registers
+    RET
+
+doneRead:
+    ; restores system back to previous state
+    POP di
+    POP dx
+    POP cx
+    POP bx
+    POP ax
+
+    ret
+
 
 print:
     ;Perserve values
@@ -70,7 +174,7 @@ done_print:
     RET
 
 os_boot_msg: DB 'Our OS has booted!', 0x0D, 0x0A, 0 ; boot message with new line characters '0' is a sentinel value dictating to the program that the string is over
-
+read_failure DB 'Failed to read disk!', 0x0D, 0x0A, 0
 TIMES 510-($-$$) DB 0    ; '$-$$' gives us the current size of the application and how many bytes it takes up. 
 ;510 is the location in memory we want to create data for. So esentially after the application is loaded and up till 510 bytes there will be empty data
 DW 0AA55h   ; The bios is searching for this signature it is considered the signature of a valid boot record
